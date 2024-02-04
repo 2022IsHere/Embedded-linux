cc3ffa9fc31101d656a1a72556c5e2a39b4435f8
Sebastian Sopola <sebastian.sopola@edu.turkuamk.fi>
2024-02-04 14:55:53 +0200

    NN

--- lab4/4.0/src/lab4-ledon.c
+++ lab4/4.0/src/lab4-ledon.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/time.h>
+
 
 #ifndef	CONSUMER
 #define	CONSUMER	"Consumer"
@@ -18,21 +20,44 @@ void updateFile() {
     // Get timestamp
     time_t currentTime = time(NULL);
     struct tm date = *localtime(&currentTime);
-    fprintf(file, "[LED ON: %d-%02d-%02d %02d:%02d]\n", date.tm_year + 1900, date.tm_mon + 1, date.tm_mday,
+    fprintf(file, "[MOTOR is turned ON: %d-%02d-%02d %02d:%02d]\n", date.tm_year + 1900, date.tm_mon + 1, date.tm_mday,
             date.tm_hour, date.tm_min);
 
     fclose(file);
 }
 
+// Function to initialize the timer
+void init_timer(struct timeval *timer) {
+    gettimeofday(timer, NULL);
+}
+
+// Function to reset the timer
+void reset_timer(struct timeval *timer) {
+    timer->tv_sec = 0;
+    timer->tv_usec = 0;
+}
+
+
+
 int main()
 {
+
 	char *chipname = "gpiochip0";
 	unsigned int led_line_num = 23;	// GPIO Pin #23
+	unsigned int button_line_num = 22; // GPIO Pin #22
 	unsigned int led_state = 0;	// determines state of the led, 0 at default
+	unsigned int button_state = 1;
 	struct gpiod_chip *chip;
-	struct gpiod_line *led_line;
+	struct gpiod_line *led_line, *button_line;
 	int led;
-	
+	int btn;
+
+	// initialize variables to clock button press
+	struct timeval press_start = {0};
+
+	// initialize variables for led duty cycle
+	struct timeval led_on_timer = {0};
+
 	// Setup chip
 	chip = gpiod_chip_open_by_name(chipname);
 	if (!chip) {
@@ -47,6 +72,20 @@ int main()
         goto close_chip;
     }
 
+	// Setup GPIO line for button
+	button_line = gpiod_chip_get_line(chip, button_line_num);
+	if (!button_line) {
+        perror("Get button line failed\n");
+        goto release_button_line;
+    }
+
+	// Check GPIO PIN 22 for input configuration. By default input has internal resistor
+	btn = gpiod_line_request_input(button_line, CONSUMER);
+	if (btn < 0) {
+		perror("Request button line as input failed\n");
+		goto release_button_line;
+	}
+
 	// Check GPIO PIN 23 for output configuration
 	led = gpiod_line_request_output(led_line, CONSUMER, 0);
 	if (led < 0) {
@@ -54,15 +93,27 @@ int main()
 		goto release_line;
 	}
 
-	// Turn LED on
-	led_state = 1;
-	led = gpiod_line_set_value(led_line, led_state);
 
-	// Update log file for debugging
-	updateFile();
+	// Fetch button value from GPIO PIN 22
+    button_state = gpiod_line_get_value(button_line);
 
-	printf("LED has been turned on! Jippii! URAAAAAS\n\n");
+	// Check water level switch
+	if (button_state == 0) {
 
+		// Turn MOTOR on		
+		led_state = !button_state;
+		led = gpiod_line_set_value(led_line, led_state);
+
+		init_timer(&press_start);
+
+		// Log the time motor is turned on
+		updateFile(); 
+
+		// keep motor running for 10 minutes by pausing the program
+		sleep(600);
+	}
+release_button_line:
+    gpiod_line_release(button_line);
 release_line:
 	gpiod_line_release(led_line);
 close_chip:
