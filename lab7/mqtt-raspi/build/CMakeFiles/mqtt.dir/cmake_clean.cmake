8dd94abdc9af9d9f281b2a395635b58beea55618
Sebastian Sopola <sebastian.sopola@edu.turkuamk.fi>
2024-03-14 12:35:20 +0200

    Lab7 mqtt-vm and mqtt-raspi set ups. Fully running

---
+++ lab7/mqtt-raspi/build/CMakeFiles/mqtt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqtt.dir/gpio.c.o"
+  "CMakeFiles/mqtt.dir/mqtt-client.c.o"
+  "mqtt"
+  "mqtt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mqtt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
